ab9469549ab2db697facb8a91c100aaa0d40d181
Sivashangari <63153736+SivashangariSivakumaran@users.noreply.github.com>
2021-08-03 11:50:38 +0530

    Route of forms added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
- import { BrowserRouter as Router,Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router,Route, Switch } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
 import Dashboard from './screens/hospital/Dashboard'
 import AdminDashboard from './screens/healthMinistry/AdminDashBoard'
+import Login from './screens/Login'
+import AddHospital from './screens/healthMinistry/AddHospital'
+import AddPatient from './screens/hospital/AddPatient'
+import AddHospitalStaff from './screens/hospital/AddHospitalStaff'
+import './bootstrap.min.css';
+import './App.css';
+
 
 const App = () =>{
   return (
@@ -15,7 +21,11 @@ const App = () =>{
         {/* <Container> */}
           <Route path='/hospital/dashboard' component={Dashboard}/>
           <Route path='/healthMinistry/dashboard' component={AdminDashboard}/>
+          <Route path='/healthMinistry/addHospital' component={AddHospital}/>
+          <Route path='/hospital/addHospitalStaff' component={AddHospitalStaff}/>
+          <Route path='/hospital/addPatient' component={AddPatient}/>
           <Route path='/' component={HomeScreen} exact/>
+          <Route path='/login' component={Login}/>
         {/* </Container> */}
       </main>
       <Footer />
