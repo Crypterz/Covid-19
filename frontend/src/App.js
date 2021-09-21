6a9e29246335a20c1aaec6cd0074b59016f37ae8
Sivashangari <63153736+SivashangariSivakumaran@users.noreply.github.com>
2021-09-21 20:48:40 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ import Dashboard from './screens/hospital/Dashboard'
 import Patient from './screens/hospital/Patients'
 import PatientProfile from './screens/hospital/PatientProfile'
 import AdminDashboard from './screens/healthMinistry/AdminDashBoard'
-//import Login from './screens/Login'
+
 import AddHospital from './screens/healthMinistry/AddHospital'
 import AddPatient from './screens/hospital/AddPatient'
 import AddHospitalStaff from './screens/hospital/AddHospitalStaff'
@@ -23,7 +23,7 @@ import './App.css';
 import AddWard from './screens/hospital/wards/AddWards'
 import Ward from './screens/hospital/wards/Ward'
 import WardsList from './screens/hospital/wards/WardsList'
-
+import SigninScreen from './screens/SigninScreen';
 
 
 const App = () =>{
@@ -44,6 +44,7 @@ const App = () =>{
           ></ToastContainer>
         {/* <Container> */}
           <switch>
+           <Route path='/signin' component={SigninScreen}/>
             <Route path='/hospital/dashboard' component={Dashboard}/>
             <Route path='/hospital/patients' component={Patient}/>
             <Route path='/hospital/patientProfile/:id' component={PatientProfile}/>
