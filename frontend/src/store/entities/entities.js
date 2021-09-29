c3849bce9d15f5a84b37f92ba33084c9eaacaba5
Sivashangari <63153736+SivashangariSivakumaran@users.noreply.github.com>
2021-09-29 13:24:12 +0530

    Update entities.js

--- frontend/src/store/entities/entities.js
+++ frontend/src/store/entities/entities.js
@@ -1,7 +1,8 @@
 import { combineReducers} from "redux";
 import patientReducer from './patients'
 import pcrReducer from './pcr';
-import hospitalReducer from './hospitals'
+import hospitalReducer from './hospitals';
+import userReducer from './users';
 
 export default combineReducers({
     patients: patientReducer,
