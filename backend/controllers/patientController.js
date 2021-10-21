4f771800df28078a7d0c08cba7a3781ad57fd32b
Dakshithas <dakshitha.18@cse.mrt.ac.lk>
2021-10-21 14:03:51 +0530

    modify2

--- backend/controllers/patientController.js
+++ backend/controllers/patientController.js
@@ -5,66 +5,68 @@ const catchAsync= require('./../utils/catchAsync');
 const AppError = require('../utils/appError');
 
 
-exports.getAllPatients = async (req, res) => {
-    try{
+// exports.getAllPatients = async (req, res) => {
+//     try{
         
-        // const queryObj = {...req.query}
-        // const excludeFields = ['page','sort','limit','fields']  //FLITERRING
-        // excludeFields.forEach(el=>delete queryObj[el])
-        // console.log(queryObj)
+//         // const queryObj = {...req.query}
+//         // const excludeFields = ['page','sort','limit','fields']  //FLITERRING
+//         // excludeFields.forEach(el=>delete queryObj[el])
+//         // console.log(queryObj)
 
-        // let queryStr=JSON.stringify(queryObj)                //localhost:8000/api/v1/patients?name=Nimsl&age[gte]=7
-        // queryStr=queryStr.replace(/\b(gte|lte|gt|le)\b/g,match=>`$${match}`)
-        // console.log(JSON.parse(queryStr))
+//         // let queryStr=JSON.stringify(queryObj)                //localhost:8000/api/v1/patients?name=Nimsl&age[gte]=7
+//         // queryStr=queryStr.replace(/\b(gte|lte|gt|le)\b/g,match=>`$${match}`)
+//         // console.log(JSON.parse(queryStr))
 
-        // let query=Patient.find(JSON.parse(queryStr))
+//         // let query=Patient.find(JSON.parse(queryStr))
 
-        // if(req.query.sort){
-        //     const sortBy=req.query.sort.split(',').join(' ')         //localhost:8000/api/v1/patients?age[gte]=7&sort=age,no
-        //     query = query.sort(sortBy)      
-        // }else{
-        //     query = query.sort('-createdAt')
-        // }
+//         // if(req.query.sort){
+//         //     const sortBy=req.query.sort.split(',').join(' ')         //localhost:8000/api/v1/patients?age[gte]=7&sort=age,no
+//         //     query = query.sort(sortBy)      
+//         // }else{
+//         //     query = query.sort('-createdAt')
+//         // }
         
-        // if(req.query.fields){
-        //     const fields =req.query.fields.split(',').join(' ')
-        //     query.select(fields)
-        // }else{
-        //     query.select('-__v')
-        // }
+//         // if(req.query.fields){
+//         //     const fields =req.query.fields.split(',').join(' ')
+//         //     query.select(fields)
+//         // }else{
+//         //     query.select('-__v')
+//         // }
 
-        // const page =req.query.page*1 || 1
-        // const limit=req.query.limit*1 || 100
-        // const skip=(page-1)*limit
-        // query=query.skip(skip).limit(limit)                    //localhost:8000/api/v1/patients?page=2&limit=10
+//         // const page =req.query.page*1 || 1
+//         // const limit=req.query.limit*1 || 100
+//         // const skip=(page-1)*limit
+//         // query=query.skip(skip).limit(limit)                    //localhost:8000/api/v1/patients?page=2&limit=10
 
-        // if(req.query.page){
-        //     const numPatient=await Patient.countDocuments()
-        //     if(skip>=numPatient) throw new Error('The page is not exist')
-        // }
+//         // if(req.query.page){
+//         //     const numPatient=await Patient.countDocuments()
+//         //     if(skip>=numPatient) throw new Error('The page is not exist')
+//         // }
         
 
-        // const patients=await Patient.find().where('name').equals('nimal')
-        const features=new APIfunctions(Patient.find()
-        // .populate({
-        //     path:'pcrTest',
-        //     select:'-slug -age'
-        // })
-        ,req.query).filter().sort().select()
-        const patients=await features.query
-        res.status(200).json({
-        status: 'success',
-        requestedAt: req.requestTime,
-        results: patients.length,
-        data: {patients}
-    });
-    }catch(err){                 //if schema doent stisfy error may occur VALIDATIO ERROR
-        res.status(404).json({
-            status:'fail',
-            message:err
-        })
-    }
-};
+//         // const patients=await Patient.find().where('name').equals('nimal')
+//         const features=new APIfunctions(Patient.find()
+//         // .populate({
+//         //     path:'pcrTest',
+//         //     select:'-slug -age'
+//         // })
+//         ,req.query).filter().sort().select()
+//         const patients=await features.query
+//         res.status(200).json({
+//         status: 'success',
+//         requestedAt: req.requestTime,
+//         results: patients.length,
+//         data: {patients}
+//     });
+//     }catch(err){                 //if schema doent stisfy error may occur VALIDATIO ERROR
+//         res.status(404).json({
+//             status:'fail',
+//             message:err
+//         })
+//     }
+// };
+
+
 
 
 exports.getPatient = catchAsync(async (req, res,next) => {
@@ -88,6 +90,20 @@ exports.getPatient = catchAsync(async (req, res,next) => {
     // }
 })
 
+
+exports.getAllPatients = catchAsync(async (req, res,next) => {
+    const patients=await Patient.find().populate({
+        path:'pcrTest medicalHistory user',
+        select: '-__v -name -passwordResetToken'
+    })    //Patient.findOne({_id:req.params.id})
+    if(!patients){
+        return next(new AppError("No patient found",404))    //used return statement to avoid executing code below
+    }
+    res.status(200).json({
+    status: 'success',
+    data: {patients}
+    });
+})
 exports.createPatient= catchAsync(async (req,res)=>{
     console.log('pppppppppppppppppppppppppppppppppppppppppp')
     console.log(req.body)
