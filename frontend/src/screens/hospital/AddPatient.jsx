41ceddead9d35785944c14e737e17d4caac3b765
Sivashangari <63153736+SivashangariSivakumaran@users.noreply.github.com>
2021-11-08 17:45:08 +0530

    Update AddPatient.jsx

--- frontend/src/screens/hospital/AddPatient.jsx
+++ frontend/src/screens/hospital/AddPatient.jsx
@@ -39,16 +39,16 @@ export default function AddPatient(props) {
         return reg.test(city)
     }
 
-    function validateNIC(nic) {
+    function validateNIC(nicno) {
         const regex = /^([0-9]{9})(V|v)$/;
         const regex2 = /^([0-9]{12})$/;
     
-        if (regex.test(nic)) {
-            return regex.test(nic);
+        if (regex.test(nicno)) {
+            return regex.test(nicno);
         }
     
-        else if (regex2.test(nic)) {
-            return regex2.test(nic);
+        else if (regex2.test(nicno)) {
+            return regex2.test(nicno);
         }
     }
 
@@ -69,7 +69,7 @@ export default function AddPatient(props) {
     const [passwordConfirm, setPasswordConfirm] = useState('');
     const [nicno, setNicno] = useState('');
     const [person, setPerson] = useState('');
-    console.log(birthday)
+    
     const submitHandler = (e) => {
       e.preventDefault();
       let user= {
@@ -108,6 +108,9 @@ export default function AddPatient(props) {
         else if (!validateContact(contact)) {
             alert("Enter valid telephone number");
         }
+        else if (!validateNIC(nicno)) {
+            alert("Enter valid NIC number");
+        }
         else if (!validateCity(city)) {
             alert("City should not include digits");
         }
@@ -128,7 +131,7 @@ export default function AddPatient(props) {
         if(userAddedStatus.userAdded && userState){
             setUserState(false)
             dispatch(toastAction({ message: "Patient Added Successfully", type: 'info' }))
-            //window.location.href = "/healthMinistry/hospital";
+            window.location.href = "/";
         }
         // else{
         //     dispatch(toastAction({ message: "Pattient Adding Failed", type: 'error' }))
@@ -136,17 +139,15 @@ export default function AddPatient(props) {
     },[userAddedStatus])
 
     return (
-        // <Box sx={{ width: 800, height: 1600, backgroundColor: 'primary.main'}}>
-            <div className="container-fluid">
-                    <div className="col-10 mx-auto banner text-center">
+        <Container fluid > 
+                <Box sx={{ bgcolor: '#cfe8fc', height: '1350px' }}>
+                <div className="col-10 mx-auto banner text-center"><br/>
                     <h3 className="text-capitalize">
-                            <strong className="banner-title">Want to Add HOSPITAL ADMIN?</strong></h3></div>
-    
-            <Container >
+                            <strong className="banner-title">Want to Add NEW PATIENT?</strong></h3></div>
                 <Form className="form" onSubmit={submitHandler}>
     
                     <Form.Group as={Col}  controlId='firstName'>
-                    <Form.Label className = 'form-label' >FIRST NAME:</Form.Label>
+                    <Form.Label className = 'form-label' ><strong>FIRST NAME:</strong> </Form.Label>
                     <Col sm={10}>
                     <Form.Control 
                         type='text'
@@ -159,7 +160,7 @@ export default function AddPatient(props) {
                     </Form.Group>
                     
                     <Form.Group as={Col} controlId='lastName'>
-                    <Form.Label className = 'form-label'>LAST NAME:</Form.Label>
+                    <Form.Label className = 'form-label'> <strong>LAST NAME:</strong></Form.Label>
                     <Col sm={10}>
                     <Form.Control 
                         type='text'
@@ -172,7 +173,7 @@ export default function AddPatient(props) {
                     </Form.Group>
 
                     <Form.Group as={Col} controlId='email'>
-                    <Form.Label class="float-left" className = 'form-label'>EMAIL ADDRESS:</Form.Label>
+                    <Form.Label class="float-left" className = 'form-label'><strong>EMAIL ADDRESS:</strong></Form.Label>
                     <Col sm={10}>
                     <Form.Control 
                         type='email'
@@ -185,7 +186,7 @@ export default function AddPatient(props) {
                     </Form.Group>
 
                     <Form.Group as={Col} controlId='birthday'>
-                    <Form.Label class="float-left" className = 'form-label'>DATE OF BIRTH:</Form.Label>
+                    <Form.Label class="float-left" className = 'form-label'><strong>DATE OF BIRTH:</strong></Form.Label>
                     <Col sm={10}>
                     <Form.Control 
                         type='date'
@@ -199,7 +200,7 @@ export default function AddPatient(props) {
                     </Form.Group>
 
                     <Form.Group as={Col} controlId='contact'>
-                    <Form.Label class="float-left" className = 'form-label' >CONTACT NUMBER:</Form.Label>
+                    <Form.Label class="float-left" className = 'form-label' ><strong>CONTACT NUMBER:</strong></Form.Label>
                     <Col sm={10}>
                     <Form.Control 
                         type='number'
@@ -214,7 +215,7 @@ export default function AddPatient(props) {
                     </Form.Group>
 
                     <Form.Group as={Col} controlId='nicno'>
-                            <Form.Label class="float-left" className = 'form-label' >NIC NUMBER:</Form.Label>
+                            <Form.Label class="float-left" className = 'form-label' ><strong>NIC NUMBER:</strong></Form.Label>
                             <Col sm={10}>
                             <Form.Control 
                                 type='text'
@@ -228,51 +229,58 @@ export default function AddPatient(props) {
                     </Form.Group>
 
                     <Form.Group as={Col} controlId="line1">
-                                <Form.Label class="float-left" className = 'form-label'>ADDRESS LINE 1:</Form.Label>
+                                <Form.Label class="float-left" className = 'form-label'><strong>ADDRESS:</strong></Form.Label>
                                 <Col sm={10}>
-                                <Form.Control
-                                type="text"
-                                id="line1"
-                                required 
-                                placeholder='Enter Line 1'
-                                size='sm'
-                                onChange={(e) => setLine1(e.target.value)}/></Col>
+                                    <Form.Control
+                                    type="text"
+                                    id="line1"
+                                    required 
+                                    placeholder='Enter Line 1'
+                                    size='sm'
+                                    onChange={(e) => setLine1(e.target.value)}/>
+                                </Col><br/>
+                                <Col sm={10}>
+                                    <Form.Control
+                                    type="text"
+                                    id="line2"
+                                    size='sm'
+                                    placeholder='Enter Line 2'
+                                    onChange={(e) => setLine2(e.target.value)}/></Col>
                     </Form.Group>
 
-                    <Form.Group as={Col} controlId="line2">
+                    {/* <Form.Group as={Col} controlId="line2">
                                 <Form.Label class="float-left" className = 'form-label'>ADDRESS LINE 2:</Form.Label>
                                 <Col sm={10}>
                                 <Form.Control
                                 type="text"
                                 id="line2"
-                                required 
                                 size='sm'
                                 placeholder='Enter Line 2'
                                 onChange={(e) => setLine2(e.target.value)}/></Col>
-                    </Form.Group>
+                    </Form.Group> */}
 
                     <Form.Group as={Col} controlId="formCity">
-                                <Form.Label class="float-left" className = 'form-label'>CITY:</Form.Label>
+                                <Form.Label class="float-left" className = 'form-label'><strong>CITY:</strong></Form.Label>
                                 <Col sm={10}>
-                                <Form.Control
-                                type="text"
-                                id="city"
-                                required 
-                                size='sm'
-                                placeholder='Enter City'
-                                onChange={(e) => setCity(e.target.value)}/></Col>
+                                    <Form.Control
+                                    type="text"
+                                    id="city"
+                                    required 
+                                    size='sm'
+                                    placeholder='Enter City'
+                                    onChange={(e) => setCity(e.target.value)}/>
+                                </Col>
                     </Form.Group>
 
                     <Form.Group as={Col}  controlId="formProvince">
-                                <Form.Label class="float-left" className = 'form-label' >PROVINCE:</Form.Label>
+                                <Form.Label class="float-left" className = 'form-label' ><strong>PROVINCE:</strong></Form.Label>
                                 <Col sm={10}>
                                 <Form.Control 
                                 as="select" 
                                 id="province" 
-                                size='sm'
                                 onChange={(e) => setProvince(e.target.value)}
                                 required>
-                                    <option value="" disabled selected >choose </option>
+                                    <option value="" disabled selected>Choose </option>
                                     <option value="Central">Central Province</option>
                                     <option value="Eastern">Eastern Province</option>
                                     <option value="Northern">Northern Province</option>
@@ -287,8 +295,8 @@ export default function AddPatient(props) {
                                 </Form.Group>
 
                     <Form.Group as={Col} controlId="password">
-                                <Form.Label class="float-left" className = 'form-label'>PASSWORD:</Form.Label>
-                                <Col sm={10}>
+                            <Form.Label class="float-left" className = 'form-label'><strong>PASSWORD:</strong></Form.Label>
+                            <Col sm={10}>
                                 <Form.Control
                                 type="password"
                                 id="password"
@@ -296,12 +304,21 @@ export default function AddPatient(props) {
                                 size='sm'
                                 placeholder='Enter Password'
                                 onChange={(e) => setPassword(e.target.value)}/>
-                    <small>*Password should atleast be of 6 characters <br/>
+                            </Col><br/>
+                            <Col sm={10}>
+                                <Form.Control
+                                type="password"
+                                id="passwordConfirm"
+                                required 
+                                size='sm'
+                                placeholder='Re enter Password'
+                                onChange={(e) => setPasswordConfirm(e.target.value)}/>
+                                <small>*Password should atleast be of 6 characters <br/>
                            *Should include atleast one simple letter, capital letter, special character and number </small> 
-                           </Col>
+                            </Col>
                     </Form.Group>
 
-                    <Form.Group as={Col} controlId="passwordConfirm">
+                    {/* <Form.Group as={Col} controlId="passwordConfirm">
                                 <Form.Label class="float-left" className = 'form-label'>RE-ENTER PASSWORD:</Form.Label>
                                 <Col sm={10}>
                                 <Form.Control
@@ -311,26 +328,15 @@ export default function AddPatient(props) {
                                 size='sm'
                                 placeholder='Re enter Password'
                                 onChange={(e) => setPasswordConfirm(e.target.value)}/></Col>
-                    </Form.Group>
-
-                    <Form.Group as={Col} controlId='role'>
-                        <Form.Label class="float-left" className = 'form-label'>ROLE:</Form.Label>
-                        <Col sm={10}>
-                        <Form.Control 
-                            type='text'
-                            id='role' 
-                            size='sm'
-                            value='Hospital Admin'
-                        /></Col>
-                    </Form.Group>
+                    </Form.Group> */}
                         <div>
-                        <Button variant="primary" type="submit">ADD HOSPITAL ADMIN</Button>
+                        <Button variant="primary" type="submit">ADD PATIENT</Button>
                         </div>
                 
                 </Form>
+                </Box> 
             </Container>
-            </div>
-            // </Box> 
+            // </div>
             
         );
     }
